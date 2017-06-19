53e267ee2456dd00c6d8e37300f581b10085f95b
Dario Vranjković <vranja33@gmail.com>
2017-06-19 15:33:11 +0200

    First project - Attack method

--- First project/app.js
+++ First project/app.js
@@ -8,8 +8,36 @@ new Vue({
     methods: {
         startGame: function () {
             this.playerHealth = 100;
-            this.monsterHealth =  100;
+            this.monsterHealth = 100;
             this.gameIsRunning = true;
+        },
+        attack: function () {
+            this.monsterHealth -= this.calculateDamage(3, 10);
+
+            if (this.monsterHealth <= 0) {
+                alert('You won!');
+                this.gameIsRunning = false;
+                return;
+            }
+            
+            this.playerHealth -= this.calculateDamage(5, 12);
+            
+            if (this.playerHealth <= 0) {
+                alert('You lost!');
+                this.gameIsRunning = false;
+            }
+        },
+        specialAttack: function () {
+
+        },
+        heal: function () {
+
+        },
+        giveUp: function () {
+
+        },
+        calculateDamage: function (min, max){
+            return Math.max(Math.floor(Math.random() * max) + 1, min);
         }
     }
 });
